3d5c9b8b927a2f217d204042eb256eb4dededa5b
Rappich <fredrik.martensson@chasacademy.se>
2025-04-03 15:30:36 +0200

    working on navbar

--- react-tsx-portfolio/src/App.tsx
+++ react-tsx-portfolio/src/App.tsx
@@ -12,8 +12,8 @@ function App() {
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoading(true)} />}{" "}
       <div
         className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"
-          } bg-black text-gray-100`}
-      >
+          } bg-white text-gray-100`}>
+
 
       </div>
 
